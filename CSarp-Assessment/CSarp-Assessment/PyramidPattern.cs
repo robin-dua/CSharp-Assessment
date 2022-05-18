cac369db895aa91d2ce30be179538c3cf4ff63ac
Robin Dua <103138471+robin-dua@users.noreply.github.com>
2022-05-18 06:54:55 +0530

    Initial Commit

--- CSarp-Assessment/CSarp-Assessment/PyramidPattern.cs
+++ CSarp-Assessment/CSarp-Assessment/PyramidPattern.cs
@@ -1,4 +1,11 @@
-﻿using System;
+/* Write a program in C# Sharp to make such a pattern like a pyramid with an asterisk. */
+/* Expected output:
+   *
+  * * 
+ * * *
+* * * *  */
+
+using System;
 
 namespace CSarp_Assessment
 {
